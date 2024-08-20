aef0cdd964d0e1108828d4e35d41748ccd9f1237
iSAD <bradx2713@gmail.com>
2024-08-20 14:48:54 +0200

    Initial Commit

---
+++ Social Media Sushi/Social_Media_SushiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Social_Media_SushiApp.swift
+//  Social Media Sushi
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/08/20.
+//
+
+import SwiftUI
+
+@main
+struct Social_Media_SushiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
